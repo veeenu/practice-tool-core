1cbc573acd58e6c050ba4472d1da5e9306fc26e4
Andrea Venuta <andrea@impl.stream>
2025-08-02 11:29:26 +0200

    Add multiple versions, add, delete, config edit support

--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -2,14 +2,18 @@ use std::path::{Path, PathBuf};
 #[cfg(unix)]
 use std::process::Child;
 use std::process::Command;
-use std::thread;
 use std::time::Duration;
+use std::{fs, thread};
 
 use anyhow::{Context, Result, bail};
-use eframe::{App, NativeOptions};
-use egui::{CentralPanel, ViewportBuilder};
+use eframe::{App, Frame, NativeOptions};
+use egui::{
+    Align, Button, CentralPanel, ComboBox, Grid, Id, Label, Layout, Modal, Pos2, Rect, Response,
+    Sense, Sides, Vec2, ViewportBuilder,
+};
 use rfd::FileDialog;
 use steamworks::{AppId, Client};
+use toml_edit::{DocumentMut, value};
 
 #[cfg(unix)]
 struct Compat {
@@ -68,6 +72,7 @@ impl Compat {
     }
 }
 
+// Windows compat is a no-op.
 #[cfg(windows)]
 pub struct Compat;
 
@@ -82,37 +87,70 @@ impl Compat {
     }
 }
 
-struct Launcher {
-    app_path: String,
-    tool_path: PathBuf,
-    compat: Compat,
+pub struct LauncherConfig {
+    /// The game's Steam appid (e.g. 1245620)
+    pub game_appid: u32,
+    /// The name of the game's executable file (e.g. `eldenring.exe`)
+    pub game_exe_name: &'static str,
+    /// The path to the executable from the game's installation directory
+    /// (e.g. `Game/eldenring.exe`)
+    pub game_exe_subpath: &'static str,
+    /// The name of the tool's configuration file
+    /// (e.g. `jdsd_er_practice_tool.toml`)
+    pub config_file_name: &'static str,
+    pub tool_exe_path: &'static str,
 }
 
-impl Launcher {
-    fn for_appid(
-        app_id: u32,
-        tool_path: impl AsRef<Path>,
-        app_path: Option<String>,
-        app_subpath: &str,
-    ) -> Result<Self> {
-        let app_id = AppId(app_id);
+#[derive(Clone, Copy)]
+enum Action {
+    DoNothing,
+    DeleteVersion,
+}
+
+struct LauncherUi {
+    chosen_game_path: usize,
+    game_paths: Vec<String>,
+    tool_path: PathBuf,
+    compat: Compat,
+    launcher_config: LauncherConfig,
+
+    confirm_msg: Option<(&'static str, Action)>,
+    error_msg: Option<String>,
+}
+
+impl LauncherUi {
+    fn new(launcher_config: LauncherConfig) -> Result<Self> {
+        let app_id = AppId(launcher_config.game_appid);
         let client = Client::init_app(app_id)?;
         let compat = Compat::new(&client)?;
-        let tool_path = tool_path.as_ref().to_path_buf();
+        let tool_path = PathBuf::from(launcher_config.tool_exe_path);
 
-        let app_path = app_path.unwrap_or_else(|| {
-            PathBuf::from(client.apps().app_install_dir(app_id))
-                .join(app_subpath)
-                .to_string_lossy()
-                .to_string()
-        });
+        let game_path = PathBuf::from(client.apps().app_install_dir(app_id))
+            .join(launcher_config.game_exe_subpath)
+            .to_string_lossy()
+            .to_string();
+        let mut game_paths = vec![game_path];
 
-        Ok(Self { app_path, tool_path, compat })
+        let toml = fs::read_to_string(launcher_config.config_file_name)?;
+        if let Some(paths) = toml.parse::<DocumentMut>()?["launcher"]["paths"].as_array() {
+            game_paths.extend(paths.into_iter().filter_map(|el| el.as_str()).map(|s| s.to_owned()));
+        }
+
+        Ok(Self {
+            game_paths,
+            tool_path,
+            compat,
+            chosen_game_path: 0,
+            confirm_msg: None,
+            error_msg: None,
+            launcher_config,
+        })
     }
 
     fn launch(&self) -> Result<()> {
-        println!("Launching {:?}", self.app_path);
-        let _ = self.compat.launch(&self.app_path, "run");
+        let game_path = &self.game_paths[self.chosen_game_path];
+        println!("Launching {:?}", game_path);
+        let _ = self.compat.launch(game_path, "run");
         thread::sleep(Duration::from_millis(5000));
         self.launch_tool()
     }
@@ -122,29 +160,115 @@ impl Launcher {
 
         Ok(())
     }
+
+    fn confirm_modal(&mut self, ctx: &egui::Context, ui: &egui::Ui) {
+        let resp = if let Some(&(msg, action)) = self.confirm_msg.as_ref() {
+            Modal::new(Id::new("confirm_modal")).show(ctx, |ui| {
+                ui.label("Delete this game version?");
+                Sides::new().show(
+                    ui,
+                    |_| {},
+                    |ui| {
+                        if ui.button("No").clicked() {
+                            Some(Action::DoNothing)
+                        } else if ui.button("Yes").clicked() {
+                            Some(action)
+                        } else {
+                            None
+                        }
+                    },
+                )
+            })
+        } else {
+            return;
+        };
+
+        if let ((), Some(action)) = resp.inner {
+            self.confirm_msg = None;
+            match action {
+                Action::DoNothing => {},
+                Action::DeleteVersion => self.delete_game_version(),
+            }
+        }
+    }
+
+    fn error_modal(&mut self, ctx: &egui::Context, ui: &egui::Ui) {
+        let resp = if let Some(e) = self.error_msg.as_ref() {
+            Modal::new(Id::new("modal_error")).show(ctx, |ui| {
+                ui.heading("Error");
+                ui.add(Label::new(e).wrap());
+                ui.button("Ok")
+            })
+        } else {
+            return;
+        };
+
+        if resp.inner.clicked() {
+            self.error_msg = None;
+        }
+    }
+
+    fn add_game_version(&mut self) {
+        if let Some(path) = FileDialog::new().pick_file() {
+            if path.ends_with(self.launcher_config.game_exe_subpath) {
+                let path = path.to_string_lossy().to_string();
+                self.game_paths.push(path);
+            } else {
+                self.error_msg =
+                    Some(format!("This is not a path to the game's executable:\n{path:?}"));
+            }
+        }
+    }
+
+    fn delete_game_version(&mut self) {
+        println!("Deleting {}", self.chosen_game_path);
+        if self.game_paths.len() > 1 {
+            self.game_paths.remove(self.chosen_game_path);
+            self.chosen_game_path = usize::min(self.chosen_game_path, self.game_paths.len() - 1);
+        } else {
+            self.error_msg = Some("There must be at least one game version added.".to_string());
+        }
+    }
 }
 
-impl App for Launcher {
-    fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
+impl App for LauncherUi {
+    fn update(&mut self, ctx: &egui::Context, _: &mut Frame) {
         CentralPanel::default().show(ctx, |ui| {
-            ui.horizontal(|ui| {
-                ui.label("Path to game:");
-                ui.add(egui::TextEdit::singleline(&mut self.app_path).interactive(false));
-                if ui.button("Browse...").clicked() {
-                    if let Some(path) = FileDialog::new().pick_file() {
-                        self.app_path = path.to_string_lossy().to_string();
-                    }
+            self.confirm_modal(ctx, ui);
+            self.error_modal(ctx, ui);
+
+            ui.label("Choose a game version:");
+            ui.with_layout(Layout::right_to_left(Align::Min), |ui| {
+                if ui.button("\u{1f5d1}").clicked() {
+                    self.confirm_msg =
+                        Some(("Delete the selected version?", Action::DeleteVersion));
                 }
+
+                if ui.button("+").clicked() {
+                    self.add_game_version();
+                }
+
+                ComboBox::from_id_salt("Game version")
+                    .width(ui.available_width())
+                    .truncate()
+                    .selected_text(&self.game_paths[self.chosen_game_path])
+                    .show_index(ui, &mut self.chosen_game_path, self.game_paths.len(), |i| {
+                        &self.game_paths[i]
+                    });
             });
 
-            ui.horizontal(|ui| {
-                if ui.button("Launch").clicked() {
+            ui.vertical_centered(|ui| {
+                let aw = ui.available_width();
+
+                if ui.add_sized([aw * 0.4, 0.0], Button::new("Launch game + tool")).clicked() {
                     self.launch();
                 }
 
-                if ui.button("Launch tool").clicked() {
-                    self.launch();
+                if ui.add_sized([aw * 0.4, 0.0], Button::new("Launch tool only")).clicked() {
+                    self.launch_tool();
                 }
+
+                ui.end_row();
             });
         });
     }
@@ -161,12 +285,13 @@ fn main() -> eframe::Result {
         options,
         Box::new(|_| {
             Ok(Box::new(
-                Launcher::for_appid(
-                    1245620,
-                    "./jdsd_er_practice_tool.exe",
-                    None,
-                    "Game/eldenring.exe",
-                )
+                LauncherUi::new(LauncherConfig {
+                    game_appid: 1245620,
+                    game_exe_name: "eldenring.exe",
+                    game_exe_subpath: "Game/eldenring.exe",
+                    config_file_name: "tests/fixtures/jdsd_er_practice_tool.toml",
+                    tool_exe_path: "jdsd_er_practice_tool.exe",
+                })
                 .unwrap(),
             ))
         }),
